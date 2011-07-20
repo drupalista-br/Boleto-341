7e0ccb3d62b611d05eea6439e8bfeb519b3c0853
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2011-07-19 22:59:15 -0400

    Compatibility with PHP 5.2 #2

--- Banco_341.php
+++ Banco_341.php
@@ -30,12 +30,12 @@
  *    
  */
 class Banco_341 extends Boleto{
-    function setUp(){
-        $this->bank_name  = 'Itau';
+    function setUp($boleto){
+        $boleto->bank_name  = 'Itau';
     }
     
     //Implementation of Febraban free range set from position 20 to 44
-    function febraban_20to44(){
+    function febraban_20to44($boleto){
 	// 20-22 (3) -> Carteira 175
 	// 23-30 (8) -> Nosso Numero
 	// 31-31 (1) -> modulo_10( agencia . conta . carteira . nosso numero )
@@ -44,27 +44,27 @@ class Banco_341 extends Boleto{
         // 41-41 (1) -> modulo_10( agencia . conta)
         // 42-44 (3) -> Fixed zeros
         
-        $carteira        = str_pad($this->arguments['carteira'], 3, 0, STR_PAD_LEFT);
-        $nosso_numero    = str_pad($this->arguments['nosso_numero'], 8, 0, STR_PAD_LEFT);
-        $agencia         = str_pad($this->arguments['agencia'], 4, 0, STR_PAD_LEFT);
-        $conta           = str_pad($this->arguments['conta'], 5, 0, STR_PAD_LEFT);
-        $nosso_numero_dv = $this->modulo_10($agencia.$conta.$carteira.$nosso_numero);
+        $carteira        = str_pad($boleto->arguments['carteira'], 3, 0, STR_PAD_LEFT);
+        $nosso_numero    = str_pad($boleto->arguments['nosso_numero'], 8, 0, STR_PAD_LEFT);
+        $agencia         = str_pad($boleto->arguments['agencia'], 4, 0, STR_PAD_LEFT);
+        $conta           = str_pad($boleto->arguments['conta'], 5, 0, STR_PAD_LEFT);
+        $nosso_numero_dv = $boleto->modulo_10($agencia.$conta.$carteira.$nosso_numero);
         
         //positons 20 to 22
-        $this->febraban['20-44'] = $carteira;
+        $boleto->febraban['20-44'] = $carteira;
         //positons 23 to 30
-        $this->febraban['20-44'] .= $nosso_numero;
+        $boleto->febraban['20-44'] .= $nosso_numero;
         //positons 31 to 31
-        $this->febraban['20-44'] .= $nosso_numero_dv;
+        $boleto->febraban['20-44'] .= $nosso_numero_dv;
         //positons 32 to 35
-        $this->febraban['20-44'] .= $agencia;
+        $boleto->febraban['20-44'] .= $agencia;
         //positons 36 to 40
-        $this->febraban['20-44'] .= $conta;
+        $boleto->febraban['20-44'] .= $conta;
         //positons 41 to 41 and the 3 fixed zeros at the end (42 to 44)
-        $this->febraban['20-44'] .= $this->modulo_10($agencia.$conta).'000';
+        $boleto->febraban['20-44'] .= $boleto->modulo_10($agencia.$conta).'000';
         
         //save nosso numero
-        $this->computed['nosso_numero'] = $carteira.'/'.$nosso_numero.'-'.$nosso_numero_dv;
+        $boleto->computed['nosso_numero'] = $carteira.'/'.$nosso_numero.'-'.$nosso_numero_dv;
 
     }
 
